588f06b092f3cbb495d6a77f4583a08667916455
VYSHNAV <964554bbl@gmail.com>
2023-10-28 15:25:36 +0530

    profile

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+void main(){
+
+  runApp(MaterialApp(home:Center(child:Text('hello guys',style:TextStyle(color:Colors.green,fontSize: 75,decoration: TextDecoration.none )))));
+}
